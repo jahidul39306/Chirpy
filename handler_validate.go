0a68cf2fdd1cca6481db0321fa3cdb2e38ff404f
jahidul39306 <jahidulislamananto@gmail.com>
2026-04-05 20:41:00 +0200

    text cleaned feature added

--- handler_validate.go
+++ handler_validate.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strings"
 	"unicode/utf8"
 )
 
@@ -12,7 +14,7 @@ func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
 	}
 
 	type returnMsg struct {
-		Valid bool `json:"valid"`
+		Cleaned_Body string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(req.Body)
@@ -29,7 +31,22 @@ func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
 		return
 	}
+
+	bad_words := []string{"kerfuffle", "sharbert", "fornax"}
+	target_words := []string{}
+	words := strings.Split(params.Body, " ")
+
+	for _, w := range words {
+		found := slices.Contains(bad_words, strings.ToLower(w))
+		if found {
+			target_words = append(target_words, "****")
+		} else {
+			target_words = append(target_words, w)
+		}
+	}
+	new_text := strings.Join(target_words, " ")
+
 	respondWithJSON(w, http.StatusOK, returnMsg{
-		Valid: true,
+		Cleaned_Body: new_text,
 	})
 }
