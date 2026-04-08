6025f1418aa6275e0fda42f741fe464ab4937a73
jahidul39306 <jahidulislamananto@gmail.com>
2026-04-08 08:05:28 +0200

    .env file added for db connection

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/jahidul39306/Chirpy
 
 go 1.25.5
 
-require github.com/google/uuid v1.6.0 
+require github.com/google/uuid v1.6.0
+
+require (
+	github.com/joho/godotenv v1.5.1 
+	github.com/lib/pq v1.12.3 
+)
--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/jahidul39306/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("can't connect to database:", err)
+	}
 	const port = "8080"
 	const filepathRoot = "."
 
 	mux := http.NewServeMux()
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		dbQueries:      database.New(db),
+	}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerHealthz)
