75054e0af80a29b501f23525e538d316857e95c2
jahidul39306 <jahidulislamananto@gmail.com>
2026-04-10 11:53:43 +0200

    handler create chirp completed

--- handler_create_chirp.go
+++ handler_create_chirp.go
@@ -3,28 +3,34 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/jahidul39306/Chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body   string `json:"body"`
-		UserID string `json:"user_id"`
-	}
+	// type parameters struct {
+	// 	Body   string `json:"body"`
+	// 	UserID string `json:"user_id"`
+	// }
 
 	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
+	params := database.CreateChirpsParams{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Unable encode params")
+		respondWithError(w, http.StatusInternalServerError, "Unable to decode params")
 		return
 	}
 
-	type chirp struct {
-		ID        string `json:"id"`
-		CreatedAt string `json:"created_at"`
-		UpdatedAt string `json:"updated_at"`
-		Body      string `json:"body"`
-		UserID    string `json:"user_id"`
-	}
+	// type chirp struct {
+	// 	ID        string `json:"id"`
+	// 	CreatedAt string `json:"created_at"`
+	// 	UpdatedAt string `json:"updated_at"`
+	// 	Body      string `json:"body"`
+	// 	UserID    string `json:"user_id"`
+	// }
+
+	c, err := cfg.dbQueries.CreateChirps(r.Context(), params)
+
+	respondWithJSON(w, http.StatusCreated, c)
 
 }
