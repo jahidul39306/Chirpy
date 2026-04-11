4d75fb2aca536fc53d573c1fa644a132cda370a8
jahidul39306 <jahidulislamananto@gmail.com>
2026-04-11 08:46:02 +0200

    code generated for password adding to the users table

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,10 +1,11 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
 VALUES (
     gen_random_uuid(),
     NOW(), -- created_at
     NOW(), -- updated_at
-    $1  -- email
+    $1,  -- email
+    $2 -- password
 )
 RETURNING *;
 
